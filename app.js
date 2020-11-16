cc0e3e44052d963b606277ed3bda9e7f13542b32
Napoleon-Jahmiu <Napoleonjay09mail.com>
2020-11-16 12:08:44 +0100

    added AddRoutineToList and ClearFields function

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // ROUTINE CONSTRUCTOR
-function Routine(no, task, category, time) {
+function Routine(no, task, category, timefrom, timeto) {
     this.no = no
     this.task = task;
     this.category = category;
-    this.time = time;
+    this.time = timefrom;
+    this.time = timeto;
 }
 
 // UI CONSTRUCTOR
@@ -21,10 +22,20 @@ UI.prototype.addRoutineToList = function() {
     `<td>${this.no}</td>
     <td>${this.task}</td>
     <td>${this.category}</td
-    <td>${this.time}</td>
+    <td>${this.timefrom}</td>
+    <td>${this.timeto}</td>
     <td><a></td>`
 
-    lsit
+    list.appendChild(row);
+}
+
+// CLEAR FIELDS
+UI.prototype.clearFields() {
+    no = document.getElementById('no').value = '',
+        task = document.getElementById('task').value =  '',
+        category = document.getElementById('category').value = '',
+        timefrom = document.getElementById('timefrom').value = '',
+        timeto = document.getElementById('timeto').value = '';
 }
 
 // Add Event Listener
@@ -33,16 +44,23 @@ document.getElementById('routine-form').addEventListener('submit', function(e) {
     const no = document.getElementById('no').value,
         task = document.getElementById('task').value,
         category = document.getElementById('category').value,
-        time = document.getElementById('time').value;
+        timefrom = document.getElementById('timefrom').value,
+        timeto = document.getElementById('timeto').value;
     
         // instantiate a new Routine
-        const routine = new Routine(no, task, category, time)
+        const routine = new Routine(no, task, category, timefrom, timeto)
 
         // Instantiate a new UI
         const ui = new UI()
 
         // Add Routine to list 
         ui.addRoutineToList(routine);
+
+        // clear Fields
+        ui.clearFields();
+
+
+
 
 
         e.preventDefault();
