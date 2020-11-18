dfae5fda5570a8444e2f460bdebbc281dc88db9e
Napoleon-Jahmiu <Napoleonjay09mail.com>
2020-11-18 21:42:35 +0100

    added unknown functions

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // ROUTINE CONSTRUCTOR
-function Routine(no, task, category, timefrom, timeto) {
-    this.no = no
+function Routine(no, task, category, time) {
+    this.no = no;
     this.task = task;
     this.category = category;
-    this.time = timefrom;
-    this.time = timeto;
+    this.time = time;
+    // this.time = time2;
 }
 
 // UI CONSTRUCTOR
@@ -12,30 +12,29 @@ function Routine(no, task, category, timefrom, timeto) {
 function UI() {}
 
 // Add Routine to list
-UI.prototype.addRoutineToList = function() {
+UI.prototype.addRoutineToList = function(routine) {
     const list = document.getElementById('task-list');
 
     // create a row
     const row = document.createElement('tr');
     // insert cols
     row.innerHTML = 
-    `<td>${this.no}</td>
-    <td>${this.task}</td>
-    <td>${this.category}</td
-    <td>${this.timefrom}</td>
-    <td>${this.timeto}</td>
-    <td><a></td>`
+    `<td>${routine.no}</td>
+    <td>${routine.task}</td>
+    <td>${routine.category}</td>
+    <td>${routine.time}</td>
+    <td><a href="#" class="delete">X</a></td>`
 
     list.appendChild(row);
 }
 
 // CLEAR FIELDS
-UI.prototype.clearFields() = function(e) {
+UI.prototype.clearFields = function(e) {
         document.getElementById('no').value = '';
         document.getElementById('task').value =  '';
         document.getElementById('category').value = '';
-        document.getElementById('timefrom').value = '';
-        document.getElementById('timeto').value = '';
+        document.getElementById('time').value = '';
+        // document.getElementById('time2').value = '';
 }
 
 // Add Event Listener
@@ -44,11 +43,11 @@ document.getElementById('routine-form').addEventListener('submit', function(e) {
     const no = document.getElementById('no').value,
         task = document.getElementById('task').value,
         category = document.getElementById('category').value,
-        timefrom = document.getElementById('timefrom').value,
-        timeto = document.getElementById('timeto').value;
+        timefrom = document.getElementById('time').value;
+        // timeto = document.getElementById('time2').value;
     
         // instantiate a new Routine
-        const routine = new Routine(no, task, category, timefrom, timeto)
+        const routine = new Routine(no, task, category, time);
  
         // Instantiate a new UI
         const ui = new UI()
@@ -59,12 +58,6 @@ document.getElementById('routine-form').addEventListener('submit', function(e) {
         // clear Fields
         ui.clearFields();
 
-
-
-
-
         e.preventDefault();
-
-        console.log(routine);
 })
 
